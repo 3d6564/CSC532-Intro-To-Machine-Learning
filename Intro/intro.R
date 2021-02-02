b17321bb9f52be91be26a92f0c516252049550f8
crobinson89 <coryscottrobinson@gmail.com>
2021-02-02 17:59:33 -0500

    Added comments

--- Intro/intro.R
+++ Intro/intro.R
@@ -1,10 +1,15 @@
-subject_name<-c("John Doe", "Jane Doe", "Steve Graves")
-temperature <-c(98.1, 98.6, 101.4)
-flu_status<-c(FALSE, FALSE, TRUE)
-gender <-factor(c("MALE", "FEMALE", "MALE"))
-blood <-factor(c("O", "AB", "A"), levels = c("A", "B", "AB", "O"))
-symptoms <-factor(c("SEVERE", "MILD", "MODERATE"),levels = c("MILD", "MODERATE", "SEVERE"),ordered = TRUE)
-pt_data<-data.frame(subject_name, temperature, flu_status, gender, blood, symptoms, stringsAsFactors= FALSE)
+# Creating vectors
+subject_name <- c("John Doe", "Jane Doe", "Steve Graves")
+temperature <- c(98.1, 98.6, 101.4)
+flu_status <- c(FALSE, FALSE, TRUE)
+
+# Creating factors from character vectors
+gender <- factor(c("MALE", "FEMALE", "MALE"))
+blood <- factor(c("O", "AB", "A"), levels = c("A", "B", "AB", "O"))
+symptoms <- factor(c("SEVERE", "MILD", "MODERATE"),levels = c("MILD", "MODERATE", "SEVERE"),ordered = TRUE)
+
+# Create a data frame from patient data vectors
+pt_data <- data.frame(subject_name, temperature, flu_status, gender, blood, symptoms, stringsAsFactors= FALSE)
 
 # Functions in R
 addArgs<-function(x,...)
