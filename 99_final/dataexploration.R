90a1df4af3f28139d50ada884170c035ff9b2bce
crobinson89 <coryscottrobinson@gmail.com>
2021-04-27 20:49:40 -0400

    - Drop columns for correlation - Drop columns for nulls - Train and Test subsets with SMOTE and undersampling - Naive Bayes model with no Cross Validation - Parallel processing added

--- 99_final/dataexploration.R
+++ 99_final/dataexploration.R
@@ -1,6 +1,13 @@
 library(ggplot2)
 library(forcats)
 library(dplyr)
+library(reshape2)
+library(DMwR)
+library(caret)
+library(doParallel)
+library(klaR)
+library(gmodels)
+
 options(scipen=10000)
 
 #Data Import
@@ -13,6 +20,24 @@ kdd = read.csv("99_final/data/kddcup.csv",
                col.names=kdd_header)
 
 # Basic Cleaning
+## Convert Binary and all discrete features
+kdd$land = as.factor(kdd$land)
+levels(kdd$land) = list("other"=0, "same"=1)
+
+kdd$logged_in = as.factor(kdd$logged_in)
+levels(kdd$logged_in) = list("fail"=0, "success"=1)
+
+kdd$root_shell = as.factor(kdd$root_shell)
+levels(kdd$root_shell) = list("nonroot"=0, "root"=1)
+
+kdd$su_attempted = as.factor(kdd$su_attempted)
+levels(kdd$su_attempted) = list("no"=0, "yes"=1)
+
+kdd$is_hot_login = as.factor(kdd$is_hot_login)
+levels(kdd$is_hot_login) = list("no"=0, "yes"=1)
+
+kdd$is_guest_login = as.factor(kdd$is_guest_login)
+levels(kdd$is_guest_login) = list("no"=0, "yes"=1)
 
 ## Clean event column values
 levels(kdd$eventType) = gsub("\\.", "", levels(kdd$eventType))
@@ -21,11 +46,12 @@ levels(kdd$eventType) = gsub("\\.", "", levels(kdd$eventType))
 apply(kdd, 2, function(x) length(unique(x)))
 kdd$num_outbound_cmds = NULL
 
-## Columns with nulls, no columns
+## Filter nulls
+colSums(is.na(kdd))
+kdd = kdd[complete.cases(kdd),]
 colSums(is.na(kdd))
 
-
-# Data Exploration of main non-Binary Numeric Data
+# Data Exploration of dependent variable
 kdd = kdd %>% mutate(eventCategory = case_when(eventType %in% c("back","land","neptune",
                                                                 "pod","smurf","teardrop") ~ "dos",
                                                eventType %in% c("buffer_overflow","loadmodule",
@@ -36,8 +62,45 @@ kdd = kdd %>% mutate(eventCategory = case_when(eventType %in% c("back","land","n
                                                eventType %in% c("ipsweep","nmap","portsweep",
                                                                 "satan") ~ "probe",
                                                TRUE ~ "normal"))
+kdd$eventCategory = as.factor(kdd$eventCategory)
 
-# Data Exploration of Categoric Data
+# Data Exploration
+## Correlation Matrix
+get_upper_matrix <- function(matrix){
+  matrix[lower.tri(matrix)] <- NA
+  return(matrix)
+}
+corr_mat = melt(get_upper_matrix(cor(kdd[sapply(kdd,is.numeric)])))
+ggplot(data = corr_mat, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile(color="white") +
+  scale_fill_gradient2(midpoint=0,limit=c(-1,1)) +
+  theme(axis.text.x=element_text(angle=90, vjust=.5, hjust=1))
+
+## Measure correlation to drop one
+### Drop: num_compromised, serror_rate, rerror_rate, srv_serror_rate, 
+### srv_rerror_rate, srv_count, dst_host_srv_rerror_rate, dst_host_same_srv_rate,
+### dst_host_srv_serror_rate
+corr_filt = corr_mat[abs(corr_mat$value) > .9 & 
+                       abs(corr_mat$value) < 1 & 
+                       !is.na(corr_mat$value),]
+corr_filt[order(corr_filt$Var1),]
+
+kdd$num_compromised = NULL
+kdd$serror_rate = NULL
+kdd$rerror_rate = NULL
+kdd$srv_serror_rate = NULL
+kdd$srv_rerror_rate = NULL
+kdd$srv_count = NULL
+kdd$dst_host_srv_rerror_rate = NULL
+kdd$dst_host_same_srv_rate = NULL
+kdd$dst_host_srv_serror_rate = NULL
+
+## Rebuild correlation matrix
+corr_mat = melt(get_upper_matrix(cor(kdd[sapply(kdd,is.numeric)])))
+ggplot(data = corr_mat, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile(color="white") +
+  scale_fill_gradient2(midpoint=0,limit=c(-1,1)) +
+  theme(axis.text.x=element_text(angle=90, vjust=.5, hjust=1))
 
 ## protocol_type
 ggplot(kdd, mapping=aes(x=fct_rev(fct_infreq(protocol_type)))) +
@@ -84,4 +147,56 @@ ggplot(kdd, mapping=aes(x=fct_rev(fct_infreq(eventCategory)))) +
   theme_minimal() +
   coord_flip()
 
+## src_bytes and dst_bytes
+plot(table(kdd$src_bytes))
 
+# Models
+set.seed(1)
+kdd$eventType = NULL
+
+## Train and test data - standard sampling
+train_index = createDataPartition(kdd$eventCategory, p=0.9, list=FALSE)
+kdd_train = kdd[train_index,]
+
+kdd_test = kdd[-train_index,-32]
+kdd_test_y = kdd[-train_index,32]
+
+# Under and Oversample
+## 1000 gave ~.866 accuracy
+## 10000 gave ~.907 accuracy
+table(kdd_train$eventCategory)
+kdd_smote = SMOTE(form=eventCategory~., data=kdd_train, k=5, perc.over=10000)
+table(kdd_smote$eventCategory)
+
+kdd_bind = rbind(kdd_train[kdd_train$eventCategory != "u2r",], kdd_smote[kdd_smote$eventCategory == "u2r",])
+table(kdd_bind$eventCategory)
+
+kdd_down = downSample(x=kdd_bind, y=kdd_bind$eventCategory, list=FALSE)
+table(kdd_down$eventCategory)
+
+kdd_down_x = kdd_down[,-32]
+kdd_down_y = kdd_down[,32]
+
+## Naive Bayes
+cl = makePSOCKcluster(detectCores(logical=FALSE))
+registerDoParallel(cl)
+kdd_classifier <- naiveBayes(kdd_down_x, kdd_down_y)
+test_pred <- predict(kdd_classifier, kdd_test)
+stopCluster(cl)
+
+CrossTable(test_pred, kdd_test_y, prop.chisq=FALSE, prop.t=FALSE,
+           dnn=c("predicted","actual"))
+
+## Alt Naive Bayes
+kdd_nb = train(kdd_down_x, kdd_down_y, 
+               method="nb",
+               trControl=trainControl(method="cv",
+                                      number=5,
+                                      allowParallel=TRUE,
+                                      savePredictions="all"))
+predic_nb = predict(kdd_nb, as.matrix(kdd_test))
+kdd_nb
+## Decision Tree
+
+
+## ANN
